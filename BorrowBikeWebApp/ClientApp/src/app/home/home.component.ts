62d1bfca0b48426f9332d7adfacf3aedf73b028c
Aaron Leung <leunga18@up.edu>
2019-04-04 14:46:15 -0700

    Minor changes

--- BorrowBikeWebApp/ClientApp/src/app/home/home.component.ts
+++ BorrowBikeWebApp/ClientApp/src/app/home/home.component.ts
@@ -12,6 +12,11 @@ export class HomeComponent {
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     http.get<Bike[]>(baseUrl + 'api/Bikes').subscribe(result => {
       this.bikes = result;
+      for (let bike of this.bikes) {
+        if (bike.status == "Error") {
+          this.bikes = this.bikes.filter(item => item !== bike)
+        }
+      }
     }, error => console.error(error));
   }
 }
